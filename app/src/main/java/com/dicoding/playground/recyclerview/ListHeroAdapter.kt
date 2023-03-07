4ca1f91c3410cb205168df5db0b57b11d06aa228
Irshal Firdeansyah <irshalfir@gmail.com>
2023-03-07 22:04:32 +0700

    change image loading from local resource to network with glide

--- app/src/main/java/com/dicoding/playground/recyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/dicoding/playground/recyclerview/ListHeroAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class ListHeroAdapter(
     private val listHero: ArrayList<Hero>
@@ -24,7 +25,9 @@ class ListHeroAdapter(
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, description, photo) = listHero[position]
-        holder.imgPhoto.setImageResource(photo)
+        Glide.with(holder.itemView.context)
+            .load(photo)
+            .into(holder.imgPhoto)
         holder.tvName.text = name
         holder.tvDescription.text = description
         holder.itemView.setOnClickListener {
